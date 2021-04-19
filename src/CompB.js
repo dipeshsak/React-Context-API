cc8c17c21360b10aaac542c932d0bc141e58ff53
Dipesh Sakharkar <dipeshsakharkar1@gmail.com>
2021-04-19 11:35:55 +0530

    Using useContext

--- src/CompB.js
+++ src/CompB.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import CompC from './CompC'
+import {firstName,lastName} from './App'
 
 const CompB =()=>{
+    const fname = useContext(firstName)
+    const lname = useContext(lastName)
     return(
         <div>
             <h4>I am Comp B</h4>
+            <p>My name is {fname} {lname} From Comp B </p>
             <CompC/>
 
         </div>
